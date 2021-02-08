3b9fff155028bf808835b0af1319aa51e9cd3bc5
Joe Downes-Berry <j.d.berry26@gmail.com>
2021-02-07 19:32:47 -0500

    commit tree 80c3ab7430f0502e8cb8ad19bae5655ba44cb5ca

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,32 @@
+class Genre 
+    attr_accessor :name    
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def artists
+        Artist.collect do |songs|
+            songs.artist
+        end
+    end
+
+end
+    
